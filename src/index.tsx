996833e26669f652b5de7394b8ca0a157411d16a
Nathan <hello@gougoule.ch>
2025-03-14 16:14:10 +0100

    Add .gitignore, update VSCode settings, and format code for consistency

--- src/index.tsx
+++ src/index.tsx
@@ -1,49 +1,57 @@
 // @ts-nocheck
 import { serve } from "bun";
-import { JSDOM } from "jsdom"
+import { JSDOM } from "jsdom";
 
 import index from "./index.html";
 
 const server = serve({
-  routes: {
-    "/api/wiki/:name": async (req) => {
-      const name = req.params.name;
-      let response = await fetchWikipediaHTML(name)
-      return new Response(response, {
-        headers: { 'Content-Type': 'text/html' }
-      });
+    routes: {
+        "/api/wiki/:name": async (req) => {
+            const name = req.params.name;
+            let response = await fetchWikipediaHTML(name);
+            return new Response(response, {
+                headers: { "Content-Type": "text/html" },
+            });
+        },
     },
-  },
 
-  development: process.env.NODE_ENV !== "production",
+    development: process.env.NODE_ENV !== "production",
 });
 
 console.log(`🚀 Server running at ${server.url}`);
 
 async function fetchWikipediaHTML(name: string): Promise<string> {
-  let html = await (await fetch(`https://en.wikipedia.org/w/index.php?title=${name}`)).text();
-  html = html.replaceAll('href="/w/', 'href="https://en.wikipedia.org/w/')
-  html = html.replaceAll('href="/wiki/', 'href="/api/wiki/')
+    let html = await (
+        await fetch(`https://en.wikipedia.org/w/index.php?title=${name}`)
+    ).text();
+    html = html.replaceAll('href="/w/', 'href="https://en.wikipedia.org/w/');
+    html = html.replaceAll('href="/wiki/', 'href="/api/wiki/');
 
-  const doc = new JSDOM(html)
+    const doc = new JSDOM(html);
 
-  let DIVsToRemove = ["vector-header-container", "vector-page-toolbar-container", "vector-dropdown", "vector-column-end", "vector-pinnable-header-toggle-button", "mw-footer-container"]
+    let DIVsToRemove = [
+        "vector-header-container",
+        "vector-page-toolbar-container",
+        "vector-dropdown",
+        "vector-column-end",
+        "vector-pinnable-header-toggle-button",
+        "mw-footer-container",
+    ];
 
-  DIVsToRemove.forEach(element => {
-    removeDivFromDOM(doc, element)
-  })
+    DIVsToRemove.forEach((element) => {
+        removeDivFromDOM(doc, element);
+    });
 
-  html = doc.serialize();
+    html = doc.serialize();
 
-
-  
-  return html
+    return html;
 }
 
 function removeDivFromDOM(DOM, className) {
-  let elementsToRemove = DOM.window.document.getElementsByClassName(className)
+    let elementsToRemove =
+        DOM.window.document.getElementsByClassName(className);
 
-  while (elementsToRemove.length > 0) {
-    elementsToRemove[0].parentNode.removeChild(elementsToRemove[0]);
-  }
+    while (elementsToRemove.length > 0) {
+        elementsToRemove[0].parentNode.removeChild(elementsToRemove[0]);
+    }
 }
