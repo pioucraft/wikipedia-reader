4886ff5180bfa9a4ef1a416463922e737083d392
Nathan <hello@gougoule.ch>
2025-03-17 08:19:20 +0100

    REMOVE EVERYTHING AND CREATE THE ALGORITHM. ALL WE NEED IS THERE

---
+++ index.js
@@ -0,0 +1,41 @@
+// ALGORITHM!!!!!!!!!!!!! DO NOT TOUCH !!!!!
+
+let links = [];
+const main = async (path, next) => {
+    let data = await (await fetch(`https://en.wikipedia.org/${path}`)).text();
+
+    links = links.concat(
+        data
+            .match(/href="\/wiki\/[^"]+"/g)
+            .map((link) => link.match(/\/wiki\/[^"]+/)[0])
+    );
+    if (next) {
+        const toFetchLinks = links;
+        let index = 0;
+        for (let link of links) {
+            if (link.startsWith("Category:")) {
+                main(link, false);
+                index++;
+                console.log(index);
+            }
+        }
+        console.log(links);
+        setTimeout(() => {
+            let counts1 = links.reduce((acc, link) => {
+                acc[link] = (acc[link] || 0) + 1;
+                return acc;
+            }, {});
+            const sortedLinks = Object.entries(counts1)
+                .sort(([, a], [, b]) => b - a)
+                .slice(0, 50)
+                .reduce((acc, [key, value]) => {
+                    acc[key] = value;
+                    return acc;
+                }, {});
+            counts1 = sortedLinks;
+            console.log(counts1);
+        }, 10000);
+    }
+};
+
+main("/wiki/Elon_Musk", true);
--- src/routes/+page.svelte
+++
@@ -1,2 +0,0 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
